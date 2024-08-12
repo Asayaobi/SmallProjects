e611fa64cb0fa33e739f10bb556b7de70440ed58
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-12 22:45:45 +0200

    update addEventListener to dicegame.js

--- dice_game.js
+++ dice_game.js
@@ -1,10 +1,19 @@
 //store result for player 1 and player 2 with random number
-let player1 = (Math.floor(Math.random()* 6))+1
-let player2 = (Math.floor(Math.random()* 6))+1
+let player1 = 1
+let player2 = 1
 
+//roll the dice
+function rollDicePlayer1() {
+    player1 = (Math.floor(Math.random()* 6))+1
+    document.querySelector(".img1").setAttribute("src",`dice_game_images/dice${player1}.png`)
+}
+function rollDicePlayer2() {
+    player2 = (Math.floor(Math.random()* 6))+1
+    document.querySelector(".img2").setAttribute("src",`dice_game_images/dice${player2}.png`)
+}
 //modify the dice image with the random number scores
-document.querySelector(".img1").setAttribute("src",`dice_game_images/dice${player1}.png`)
-document.querySelector(".img2").setAttribute("src",`dice_game_images/dice${player2}.png`)
+document.querySelector('.button1').addEventListener("click", rollDicePlayer1)
+document.querySelector('.button2').addEventListener("click", rollDicePlayer2)
 
 //modify header to display a winner
 if (player1 > player2) {
