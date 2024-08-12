50bc280f9b34c213e3b0b8786b795883b5fc4be6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-12 18:38:20 +0200

    add result to display the winner in js file

--- dice_game.js
+++ dice_game.js
@@ -12,3 +12,9 @@ let player2 = randomNumber()
 document.querySelector(".img1").setAttribute("src",`dice_game_images/dice${player1}.png`)
 document.querySelector(".img2").setAttribute("src",`dice_game_images/dice${player2}.png`)
 
+//modify header to display a winner
+if (player1 > player2) {
+    document.querySelector('h1').innerHTML = "Player 1 wins!"
+} else if (player2 > player1) {
+    document.querySelector('h1').innerHTML = "Player 2 wins!"
+}
