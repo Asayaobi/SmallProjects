a532f5675d5d18e80731dae2cc8e49bd757e48cb
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-12 18:29:13 +0200

    store the result of random number function in .js

--- dice_game.js
+++ dice_game.js
@@ -1,6 +1,14 @@
+//create a function for random number
 function randomNumber() {
     let result = (Math.floor(Math.random()* 6))+1
     return result
 }
-document.querySelector(".img1").setAttribute("src",`dice_game_images/dice${randomNumber()}.png`)
-document.querySelector(".img2").setAttribute("src",`dice_game_images/dice${randomNumber()}.png`)
+
+//store result for player 1 and player 2
+let player1 = randomNumber()
+let player2 = randomNumber()
+
+//modify the dice image with the random number scores
+document.querySelector(".img1").setAttribute("src",`dice_game_images/dice${player1}.png`)
+document.querySelector(".img2").setAttribute("src",`dice_game_images/dice${player2}.png`)
+
