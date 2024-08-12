93222f0dcdd11aec125ad597f9181d5522658da6
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-12 18:23:44 +0200

    set attribute for images in js file

--- dice_game.js
+++ dice_game.js
@@ -2,3 +2,5 @@ function randomNumber() {
     let result = (Math.floor(Math.random()* 6))+1
     return result
 }
+document.querySelector(".img1").setAttribute("src",`dice_game_images/dice${randomNumber()}.png`)
+document.querySelector(".img2").setAttribute("src",`dice_game_images/dice${randomNumber()}.png`)
