36c0856bafb905670708fe0ac9a0306745be44d4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-15 20:31:44 +0200

    add switch case for playing audio in js file

--- drumskit.js
+++ drumskit.js
@@ -1,7 +1,45 @@
 
 function handleClick() {
-    var audio = new Audio('drumskit_sound/tom-1.mp3')
-    audio.play()
+    let buttonInnerHTML = this.innerHTML
+    switch (buttonInnerHTML) {
+        case "w":
+            let crash = new Audio('drumskit_sound/crash.mp3')
+            crash.play()
+            break;
+
+        case "a":
+            let tom2 = new Audio('drumskit_sound/tom-2.mp3')
+            tom2.play()
+            break;
+
+        case "s":
+            let tom1 = new Audio('drumskit_sound/tom-1.mp3')
+            tom1.play()
+            break;
+
+        case "d":
+            let snare = new Audio('drumskit_sound/snare.mp3')
+            snare.play()
+            break;
+
+        case "j":
+            let tom3 = new Audio('drumskit_sound/tom-3.mp3')
+            tom3.play()
+            break;
+
+        case "k":
+            let tom4 = new Audio('drumskit_sound/tom-4.mp3')
+            tom4.play()
+            break;
+
+        case "l":
+            let kick = new Audio('drumskit_sound/kick-bass.mp3')
+            kick.play()
+            break;
+            
+        default: console.log(buttonInnerHTML)
+            break;
+    }
 }
 
 // document.querySelector('.set').addEventListener("click",handleClick)
