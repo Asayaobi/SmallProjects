7da6dee0b6d7e309950192ebdd0289e7f94827ce
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-08-15 18:56:15 +0200

    link js file to html

--- drumskit.js
+++ drumskit.js
@@ -1,1 +1,13 @@
-alert('drums Kit!')
+
+function handleClick() {
+    var audio = new Audio('drumskit_sound/tom-1.mp3')
+    audio.play()
+}
+
+// document.querySelector('.set').addEventListener("click",handleClick)
+let length = document.getElementsByClassName("drum").length
+console.log(length)
+
+for (let i = 0; i < length; i++) {
+    document.querySelectorAll('button')[i].addEventListener("click",handleClick)
+}
