39bfd0bc46ea4dbe0f3727c3deeaead1767e7c8e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-05 18:42:04 +0100

    add comments to guess my number.js

--- guess_my_number.js
+++ guess_my_number.js
@@ -2,28 +2,22 @@
 
 //keep the higest score
 let highScore = 0
-
 //score count
 let score = 20
-
 // create a number with math random
 let answer =  Math.ceil(Math.random()*20)
-console.log(answer)
 
 //user click check to start a function
-
 document.querySelector('.check').addEventListener('click', function() {
     //set the guess value from the input
     const guess = Number(document.querySelector('.guess').value)
-
-    //check the answer 
     
-    // when answer has no number
+    // when the answer has no number
     if (!guess){
         document.querySelector('.message').textContent = '🔴 please guess a number'
     } 
     
-    //when answer is correct
+    //when the answer is correct
     else if (guess === answer) {
         document.querySelector('.number').textContent = answer
         document.querySelector('.number').style.width = '30rem'
@@ -40,7 +34,7 @@ document.querySelector('.check').addEventListener('click', function() {
     
     //in case the score is less than 0
     else if (score > 1) {
-        //when answer is too high - too low
+        //when the answer is too high - too low
          if (guess > answer) {
             document.querySelector('.message').textContent = '📈 Too high!'
             score--
@@ -55,24 +49,21 @@ document.querySelector('.check').addEventListener('click', function() {
 
     //display a score
     document.querySelector('.score').textContent = score
-
 })
 
 //when again is click
 document.querySelector('.again').addEventListener('click', function(){
-    //reset number
-    document.querySelector('.number').textContent = '?'
-    document.querySelector('.message').textContent = 'Start guessing...'
-
     //reset score
     score = 20
-    document.querySelector('.score').textContent = 20
-
-    //reset input
-    document.querySelector('.guess').value = ""
-
+    //recreate number
     answer =  Math.ceil(Math.random()*20)
-    console.log(answer)
+    //reset display
+    document.querySelector('.score').textContent = score
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.guess').value = ""
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.number').style.width = '15rem'
 })
 
 
