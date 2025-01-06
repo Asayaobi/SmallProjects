98ca433c83f097d7799de256f206873c965c5b55
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-06 15:25:48 +0100

    add open button logic

---
+++ modal.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const openBtn = document.querySelectorAll('.show-modal')
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeBtn = document.querySelector('.close-modal')
+console.log(openBtn) //array of 3 btns
+
+for (let i = 0; i < openBtn.length; i++){
+    openBtn[i].addEventListener('click', function(){
+        //console.log('open')
+        //modal.style.display = 'block'
+        modal.classList.remove('hidden')
+        overlay.classList.remove('hidden')
+    })
+}
+
