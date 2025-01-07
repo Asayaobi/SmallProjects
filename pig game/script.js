0f9def630f90d9ec85595d9fec8af9dca723391c
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-07 18:11:36 +0100

    create resetCurrentScore function

--- pig game/script.js
+++ pig game/script.js
@@ -43,6 +43,11 @@ const removePlayerActive = player => {
     console.log(`remove player ${player}`)
 }
 
+const resetCurrentScore = player => {
+    currentScore = 0
+    document.querySelector(`#current--${player}`).textContent = currentScore
+}
+
 //when roll dice btn is pressed
 document.querySelector('.btn--roll').addEventListener('click', function(){
     if (total0 < 100 && total1 < 100){
@@ -54,8 +59,9 @@ document.querySelector('.btn--roll').addEventListener('click', function(){
             document.querySelector(`#current--${player}`).textContent = currentScore
         } else {
             // document.querySelector(`.player--${player}`).classList.remove('player--active')
-            currentScore = 0
-            document.querySelector(`#current--${player}`).textContent = currentScore
+            // currentScore = 0
+            // document.querySelector(`#current--${player}`).textContent = currentScore
+            resetCurrentScore(player)
             removePlayerActive(player)
             player === 0 ? player = 1 : player = 0
             // document.querySelector(`.player--${player}`).classList.add('player--active')
@@ -70,8 +76,9 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
         if (player === 0){
             total0 += currentScore
             document.querySelector(`#score--${player}`).textContent = total0
-            currentScore = 0
-            document.querySelector(`#current--${player}`).textContent = currentScore
+            // currentScore = 0
+            // document.querySelector(`#current--${player}`).textContent = currentScore
+            resetCurrentScore(player)
 
             if (total0 >= 100) {
                 document.querySelector('.player--0').classList.add('player--winner')
@@ -84,8 +91,9 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
         } else {
             total1 += currentScore
             document.querySelector(`#score--${player}`).textContent = total1
-            currentScore = 0
-            document.querySelector(`#current--${player}`).textContent = currentScore
+            // currentScore = 0
+            // document.querySelector(`#current--${player}`).textContent = currentScore
+            resetCurrentScore(player)
 
             if (total1 >= 100){
                 document.querySelector('.player--1').classList.add('player--winner')
@@ -101,18 +109,20 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
 //when reset is pressed
 document.querySelector('.btn--new').addEventListener('click', function(){
     document.querySelector(`.player--${player}`).classList.remove('player--winner')
-    document.querySelector(`.player--${player}`).classList.remove('player--active')
+    // document.querySelector(`.player--${player}`).classList.remove('player--active')
+    removePlayerActive(player)
+    resetCurrentScore(player)
 
     player = 0
-    currentScore = 0
+    // currentScore = 0
     total0 = 0
     total1 = 0
     // document.querySelector(`.player--${player}`).classList.add('player--active')
     addPlayerActive(player)
-    let currentScores = document.querySelectorAll('.current-score')
-    for (i = 0; i < currentScores.length; i++){
-        currentScores[i].textContent = 0
-    }
+    // let currentScores = document.querySelectorAll('.current-score')
+    // for (i = 0; i < currentScores.length; i++){
+    //     currentScores[i].textContent = 0
+    // }
 
     let scores = document.querySelectorAll('.score')
     for (i = 0; i < scores.length; i++){
