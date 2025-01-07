4d76f3c3d9c80b5750479e63938e736a8aaa123b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-07 18:41:22 +0100

    create switchplayer function

--- pig game/script.js
+++ pig game/script.js
@@ -33,19 +33,15 @@ let currentScore = 0
 let total0 = 0
 let total1 = 0
 
-const addPlayerActive = player => {
-    document.querySelector(`.player--${player}`).classList.add('player--active')
-    console.log(`add player ${player}`)
-}
-
-const removePlayerActive = player => {
-    document.querySelector(`.player--${player}`).classList.remove('player--active')
-    console.log(`remove player ${player}`)
-}
-
 const resetCurrentScore = player => {
     currentScore = 0
     document.querySelector(`#current--${player}`).textContent = currentScore
+}
+
+const switchPlayer = () => {
+    document.querySelector(`.player--${player}`).classList.remove('player--active')
+    player === 1 ? player = 0 : player = 1
+    document.querySelector(`.player--${player}`).classList.add('player--active')
 }
 
 //when roll dice btn is pressed
@@ -58,14 +54,13 @@ document.querySelector('.btn--roll').addEventListener('click', function(){
             currentScore += number
             document.querySelector(`#current--${player}`).textContent = currentScore
         } else {
-            // document.querySelector(`.player--${player}`).classList.remove('player--active')
             // currentScore = 0
             // document.querySelector(`#current--${player}`).textContent = currentScore
             resetCurrentScore(player)
-            removePlayerActive(player)
-            player === 0 ? player = 1 : player = 0
+            // document.querySelector(`.player--${player}`).classList.remove('player--active')
+            // player === 0 ? player = 1 : player = 0
             // document.querySelector(`.player--${player}`).classList.add('player--active')
-            addPlayerActive(player)
+            switchPlayer()
         }
     }
 })
@@ -76,31 +71,23 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
         if (player === 0){
             total0 += currentScore
             document.querySelector(`#score--${player}`).textContent = total0
-            // currentScore = 0
-            // document.querySelector(`#current--${player}`).textContent = currentScore
             resetCurrentScore(player)
 
             if (total0 >= 100) {
                 document.querySelector('.player--0').classList.add('player--winner')
             } else {
-                removePlayerActive(player)
-                player === 0 ? player = 1 : player = 0
-                addPlayerActive(player)
+                switchPlayer()
             }
             
         } else {
             total1 += currentScore
             document.querySelector(`#score--${player}`).textContent = total1
-            // currentScore = 0
-            // document.querySelector(`#current--${player}`).textContent = currentScore
             resetCurrentScore(player)
 
             if (total1 >= 100){
                 document.querySelector('.player--1').classList.add('player--winner')
             } else {
-                removePlayerActive(player)
-                player === 0 ? player = 1 : player = 0
-                addPlayerActive(player)
+                switchPlayer()
             }
         }
     } 
@@ -109,20 +96,12 @@ document.querySelector('.btn--hold').addEventListener('click', function(){
 //when reset is pressed
 document.querySelector('.btn--new').addEventListener('click', function(){
     document.querySelector(`.player--${player}`).classList.remove('player--winner')
-    // document.querySelector(`.player--${player}`).classList.remove('player--active')
-    removePlayerActive(player)
-    resetCurrentScore(player)
+    document.querySelector(`.player--${player}`).classList.remove('player--active')
 
     player = 0
-    // currentScore = 0
     total0 = 0
     total1 = 0
-    // document.querySelector(`.player--${player}`).classList.add('player--active')
-    addPlayerActive(player)
-    // let currentScores = document.querySelectorAll('.current-score')
-    // for (i = 0; i < currentScores.length; i++){
-    //     currentScores[i].textContent = 0
-    // }
+    document.querySelector(`.player--${player}`).classList.add('player--active')
 
     let scores = document.querySelectorAll('.score')
     for (i = 0; i < scores.length; i++){
