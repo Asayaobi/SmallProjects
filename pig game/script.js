521264da7530b9b2b91086f5a521fe3fa49604f7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-07 14:19:45 +0100

    Create script.js

---
+++ pig game/script.js
@@ -0,0 +1,30 @@
+/*
+
+when roll dice btn is pressed
+-if total score < 100
+player 1 start
+1. math random dice number -> number
+- if number != 1
+2. display dice image
+3. add that number to current score
+- if number = 1
+1. switch player
+2. reset current score
+
+
+when hold btn is pressed
+-if total score < 100
+1. add the current score to --> total score
+2. reset current score
+3. switch player
+
+-if score >= 100
+1. player background -> black
+
+when reset is pressed
+1. reset player background 
+2. reset total score
+3. reset current score
+*/
+
+
